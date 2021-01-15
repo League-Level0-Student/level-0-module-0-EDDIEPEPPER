626dd119e0f9ea6dd1ed08a9c39e668eca648ac2
root <root@7752fa5dae1f>
2021-01-15 01:11:30 +0000

    spiral and tash me finished

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,16 +12,23 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
-		
+		 Robot steve = new Robot();
 		// Set your robot's pen down 
-		
+		steve.penDown();
 		// SPEED. Set the robot to go at max speed (100)
-
+steve.setSpeed(100);
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
 			//        Start its value as zero.
-
+int count = 0;
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
-
+while ( count < 50 ) {
+	steve.setRandomPenColor();
+	steve.move(5*count);
+	steve.turn(360/7);
+	count+=1 ;
+	steve.setPenWidth(count);
+}
+}
 			// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
 
 			// DRAW.    Move the robot (5*count) pixels
@@ -41,4 +48,4 @@ public class RobotSpiral {
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
 		
 	}
-}
+
