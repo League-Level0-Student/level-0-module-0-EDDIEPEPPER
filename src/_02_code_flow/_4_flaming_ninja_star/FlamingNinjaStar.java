1022bd15b159c29b8eb53a47271a2a38b769fad7
student <student@ip-172-31-53-88.us-west-2.compute.internal>
2021-01-21 17:00:18 -0800

    flaming ninja star finished

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,36 +10,40 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
-
+Robot ree = new Robot();
 		// Set the robot speed to 100
-		
+ree.setSpeed(100);
+		ree.penDown();
 		// Set the robot window size to 800 x 800
-
+ree.setWindowSize(800,800);
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-		
+		int count = 0;
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+while ( count < 25 ) {
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+		ree.turn(45);
 			   // MOVE           Move the robot 64 pixels
-
+ree.move(64);
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			ree.turn(-40);
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
+			ree.setPenColor(Color.red);
+			ree.move(flameSize);
 				//               Turn the robot 170 degrees
-			
+		
+			ree.turn(170);
 				//               Move the robot the distance in the variable flameSize (again)
-			
+			ree.move(flameSize);
+			ree.setPenColor(Color.black);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+			ree.turn(64);
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+		ree.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
-		
+		 count+=1 ;
 		//  End the while loop here
-		
+	}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
 		
